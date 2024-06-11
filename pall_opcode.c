a5557d0d51e93fd0e8dfb913b629df60b5eb624d
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-06-11 10:08:33 +0300

    updated these opcodes by using `line_number` to report errors

--- pall_opcode.c
+++ pall_opcode.c
@@ -16,8 +16,8 @@ void pall(stack_t **stack, unsigned int line_number)
 
 	while (current != NULL)
 	{
-		printf(" %d", current->n);
+		printf("%d\n", current->n);
 		current = current->next;
 	}
-	printf("\n");
+
 }
--- push_opcode.c
+++ push_opcode.c
@@ -3,22 +3,30 @@
 /**
  * push - pushes an element onto the top of the stack
  * @stack: pointer to the head of the stack.
- * @line_number: data to be inserted in the new element.
+ * @line_number: line number from the file where the instruction was read.
  * Return: nothing
  */
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_element = (stack_t *)malloc(sizeof(stack_t));
 
-	(void)line_number;
 	if (new_element == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	new_element->n = v_a;
-	new_element->prev = NULL;
+	if ((v_a && (atoi(v_a) != 0)) || strcmp(v_a, "0") == 0)
+	{
+		new_element->n = atoi(v_a);
+		new_element->prev = NULL;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer", line_number);
+		free(new_element);
+		exit(EXIT_FAILURE);
+	}
 
 	if (*stack != NULL)
 	{
