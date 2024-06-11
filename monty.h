6fc4c88dc88222b876e108a8e78d3a4849c325df
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-06-11 10:33:28 +0300

    included a global var: `char *v_a`

--- monty.h
+++ monty.h
@@ -1,6 +1,15 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+/* standard header files */
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+/* Global variable */
+extern char *v_a;
+
 /* data structures */
 
 /**
@@ -31,5 +40,10 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/* function declarations */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void (*opcode_check(char *opcod))(stack_t **stack, unsigned int line_number);
 
 # endif
