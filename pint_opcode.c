af6a143e6557fad9c5430b53de1adca4f3d9fc6f
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-06-12 15:57:32 +0300

    added the `pint` opcode

---
+++ pint_opcode.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * pint - prints the value at the top of the stack
+ * @stack: pointer that points to the head of the stack
+ * @line_number: reports an error[current line No. in the bytecode file]
+ * Return: nothing
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	/* if the stack is empty, exit with status: `EXIT_FAILURE` */
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	/* print it's value */
+	printf("%d\n", (*stack)->n);
+
+}
